1c387bdad0fc476bd555af8df48fdf410cdd341c
Samko <samuel.mintal3@gmail.com>
2023-03-09 22:33:04 +0100

    laid foundations for predictors

---
+++ src/predicors.py
@@ -0,0 +1,21 @@
+class K_nearest_neighbours:
+
+    # Returns Euclidean distance between 2 dataframe entries ~ vehicles
+    @staticmethod
+    def euclid_distance(elem_1, elem_2):
+        dist = 0
+
+        return dist
+
+    @staticmethod
+    def predict_price_of(entry, csv_file_path, k=3):
+        # Firstly conver entry into df row
+
+        # Then preprocess it together with csv
+
+        # Now get the closest k neighbours from the sepcifies dataset 
+
+        # Infer price based on the found closest k neighbours
+
+        # Return infered price
+        pass
